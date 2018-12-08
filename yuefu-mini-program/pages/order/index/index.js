4338d51eed697c890dc429165712532fefd32edc
mqy <1031860108@qq.com>
2018-12-08 18:20:12 +0800

    1

---
+++ yuefu-mini-program/pages/order/index/index.js
@@ -0,0 +1,113 @@
+// pages/order/index/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    active:0,
+    tabdata: ['全部', '待付款', '待发货', '待收货', '待评价'], 
+    goods_list:[1,2]  ,
+          showModal: true,
+
+  },
+  /**
+   * 弹窗
+   */
+  showDialogBtn: function () {
+    this.setData({
+      showModal: true
+    })
+  },
+  open_pay:function(){
+    wx.navigateTo({
+      url: '../gopay/gopay'
+    })
+  },
+  /**
+   * 弹出框蒙层截断touchmove事件
+   */
+  preventTouchMove: function () {
+  },
+  /**
+   * 隐藏模态对话框
+   */
+  hideModal: function () {
+    this.setData({
+      showModal: false
+    });
+  },
+  /**
+   * 对话框取消按钮点击事件
+   */
+  onCancel: function () {
+    this.hideModal();
+  },
+  /**
+   * 对话框确认按钮点击事件
+   */
+  onConfirm: function () {
+    this.hideModal();
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+  tab_clk: function (e) {
+    console.log(e.currentTarget.id)
+    this.setData({
+      active: e.currentTarget.id
+    })
+  },
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
